d03da4893bced52c067287741ac9a54edabf09df
daddej <ikharimgloria@gmail.com>
2025-07-02 18:57:22 +0800

    Feat: Add Validation handler in Api

--- user-service/src/Application/Dtos/UpdateUserDto.cs
+++ user-service/src/Application/Dtos/UpdateUserDto.cs
@@ -10,30 +10,71 @@ namespace UserService.Application.Dtos
     {
         [Required]
         public Guid Id { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+
+        [MaxLength(100)]
         public string? MiddleName { get; set; }
-        public string? Gender { get; set; }
-        public DateTime? DateOfBirth { get; set; }
+
+        [Required, MaxLength(10)]
+        public string Gender { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [MaxLength(50)]
         public string? Nationality { get; set; }
+
+        [MaxLength(50)]
         public string? CivilStatus { get; set; }
 
-        public string? Email { get; set; }
-        public string? MobileNumber { get; set; }
+        // Contact
+        [Required, EmailAddress, MaxLength(225)]
+        public string Email { get; set; }
+
+        [Required, Phone, MaxLength(225)]
+        public string MobileNumber { get; set; }
+
+        [Phone, MaxLength(225)]
         public string? AlternatePhoneNumber { get; set; }
 
-        public string? Street { get; set; }
-        public string? Barangay { get; set; }
-        public string? City { get; set; }
-        public string? Province { get; set; }
-        public string? PostalCode { get; set; }
-        public string? Country { get; set; }
+        // Address
+        [Required, MaxLength(255)]
+        public string Street { get; set; }
 
+        [MaxLength(100)]
+        public string? Barangay { get; set; }
+
+        [Required, MaxLength(100)]
+        public string City { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Province { get; set; }
+
+        [Required, MaxLength(10)]
+        public string PostalCode { get; set; }
+
+        [Required, MaxLength(100)]
+        public string Country { get; set; }
+
+        // Optional Professional Info
+        [MaxLength(100)]
         public string? Occupation { get; set; }
+
+        [MaxLength(100)]
         public string? CompanyName { get; set; }
+
+        [MaxLength(100)]
         public string? Industry { get; set; }
+
+        [Range(0, 100)]
         public int? YearOfExperience { get; set; }
 
-        public string? GovermentId { get; set; }
+        [MaxLength(100)]
+        public string? GovermentId { get; set; } 
     }
 }
